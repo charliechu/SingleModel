5b7ff148d96b92d5ec69b0cb614df0770499fe97
charliechu <charliechu@techbang.com.tw>
2011-09-07 19:20:49 +0800

    新增action controller and action view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
 class PostsController < ApplicationController
+  def index
+    
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end
 end
