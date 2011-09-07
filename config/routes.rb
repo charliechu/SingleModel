2f2bf7f242919c443fed55d60a976cba5afc197b
charliechu <charliechu@techbang.com.tw>
2011-09-07 19:22:48 +0800

    Setup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Singlemodel::Application.routes.draw do
+  resources :posts
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +50,7 @@ Singlemodel::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => "post#index"
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
