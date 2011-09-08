289d5bc3e23e27c1cb34c403e8259cf170cb7c82
charliechu <charliechu@techbang.com.tw>
2011-09-08 10:04:32 +0800

    CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,26 +4,38 @@ class PostsController < ApplicationController
   end
   
   def new
-    
+    @post = Post.new
   end
   
   def create
-    
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path
+    else
+      render :action => "new"
+    end
   end
   
   def edit
-    
+    @post = Post.find(params[:id])
   end
   
   def update
-    
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      redirect_to post_path(@post)
+    else
+      render :action => "edit"
+    end
   end
   
   def show
-    
+    @post = Post.find(params[:id])
   end
   
   def destroy
-    
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 end
