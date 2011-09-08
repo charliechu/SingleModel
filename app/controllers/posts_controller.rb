83edd27a0518e7d09aaf949e117a3247fcdde5d9
charliechu <charliechu@techbang.com.tw>
2011-09-08 11:14:19 +0800

    before filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_filter :find_post, :only => [:edit, :update, :show, :destroy]
+  
   def index
     @posts = Post.all
   end
@@ -17,11 +19,9 @@ class PostsController < ApplicationController
   end
   
   def edit
-    @post = Post.find(params[:id])
   end
   
   def update
-    @post = Post.find(params[:id])
     if @post.update_attributes(params[:post])
       redirect_to post_path(@post)
     else
@@ -30,12 +30,16 @@ class PostsController < ApplicationController
   end
   
   def show
-    @post = Post.find(params[:id])
   end
   
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
+  end
+  
+  protected
+  
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
